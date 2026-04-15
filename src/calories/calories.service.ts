fbacdde70a2818741a514d91bf28a4349927241a
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2026-04-15 15:33:33 -0300

    feat: made changes to anaylze weight progressions to reach user goal.

--- src/calories/calories.service.ts
+++ src/calories/calories.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient, type User } from '../../generated/prisma/client.js';
+import { Goal, PrismaClient, type User } from '../../generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
+
+type ActivityLevel = 'SEDENTARY' | 'LIGHT' | 'MODERATE' | 'INTENSE' | 'VERY_INTENSE';
 
 @Injectable()
 export class CaloriesService implements OnModuleDestroy {
@@ -16,14 +18,37 @@ export class CaloriesService implements OnModuleDestroy {
     });
   }
 
-  requiredCalories(user: User): number {
+  // Function that calculates minimum calories requiered by user.
+  calculateCalories(user: User, activityLevel: ActivityLevel = 'SEDENTARY'): number {
+    const minimumCalories = this.mifflinCalc(user);
+
+    const activityMultiplier: Record<ActivityLevel, number> = {
+      SEDENTARY: 1.2,
+      LIGHT: 1.375,
+      MODERATE: 1.55,
+      INTENSE: 1.725,
+      VERY_INTENSE: 1.9,
+    };
+
+    const goal : Record<Goal, number> = {
+      LOSE_FAT: 0.8,
+      MAINTAIN: 1,
+      GAIN_MUSCLE: 1.2,
+    };
+
+    return Math.round(minimumCalories * activityMultiplier[activityLevel]);
+  }
+
+  // This function analyze previous weight changes with minimum calories provided and adjusts the calories requiered.
+  analyzeWeightChange(user: User, predictedMinimumCalories: number): number {
+    
+  }
+
+  private
+
+  private mifflinCalc (user: User) : number {
     const baseCalories = 10 * user.weight_kg + 6.25 * user.height_cm - 5 * user.age;
-
-    if (user.gender === 'MALE') {
-      return Math.round(baseCalories + 5);
-    }
-
-    return Math.round(baseCalories - 161);
+    return user.gender === 'MALE' ? baseCalories + 5 : baseCalories - 161;
   }
 
   async onModuleDestroy(): Promise<void> {
