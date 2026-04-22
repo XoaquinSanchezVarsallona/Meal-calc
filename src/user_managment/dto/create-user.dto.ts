a870c4227c47844b0e3045847327830eabb4a6d4
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2026-04-22 12:09:52 -0300

    added verifications to userDTO.

--- src/user_managment/dto/create-user.dto.ts
+++ src/user_managment/dto/create-user.dto.ts
@@ -1,1 +1,51 @@
-export class UserDTO {}
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+import { Gender, Goal } from 'generated/prisma/client';
+
+export class UserDTO {
+  @IsOptional()
+  id?: string;
+
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @MinLength(8)
+  @Matches(/^(?=.*[A-Z])(?=(?:.*[^A-Za-z0-9]){2,}).+$/, {
+    message: 'password debe tener al menos una mayúscula y dos caracteres especiales',
+  })
+  password: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  height_cm: number;
+
+  @Type(() => Number)
+  @IsNumber()
+  age: number;
+
+  @IsEnum(Gender)
+  gender: Gender;
+
+  @IsString()
+  preferences: string;
+
+  @IsEnum(Goal)
+  goal: Goal;
+}
+
+export class UserResponseDTO {
+  id: string;
+  name: string;
+  password: string;
+}
